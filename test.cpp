8448aaf856a0a4af2768b0b9cd78d7e088dbc374
Zer0Fox <senior00krasnoff@gmail.com>
2020-02-15 20:54:06 +0700

    Wrote test file fore encode/decode functions. both functions works wrong

---
+++ test.cpp
@@ -0,0 +1,44 @@
+#include "mySimpleComputer.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main( void ){
+
+	int command[15];
+
+	int value = 0;
+
+	int i , j , k , s;
+
+	char name[10];
+
+	srand(time(NULL));
+	
+
+	cin >> j;
+	cin >> s;
+
+	k = sc_commandEncode(j , s, command);
+
+	cout << "memoryEncode " << k << endl;
+
+	for ( i = 0 ; i < 15 ; i++ ){
+
+		cout << command[i];
+
+	}
+
+	for ( i = 0 ; i < 15 ; i++ ){
+
+		cin >> command[i];
+
+	}
+	
+	k = sc_commandDecode( command , &j , &i);
+
+	cout << "memoryDecode " << k << " " <<j << " " << i << endl;
+
+	return 0;
+
+}
